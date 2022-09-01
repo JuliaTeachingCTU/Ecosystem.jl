e92f8b59a07e7d8c9b0c80ec8a33d6d4eb3e7707
Niklas Heim <heim.niklas@gmail.com>
2022-09-01 17:10:30 +0200

    add bench script

---
+++ examples/bench.jl
@@ -0,0 +1,15 @@
+using Ecosystem
+using BenchmarkTools
+using Random
+Random.seed!(0)
+
+sheep = Sheep(1,1,1,1,1,:female)
+sheep2 = Sheep(3001,1,1,1,1,:male)
+world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
+
+find_food(sheep,world) |> display
+#@code_warntype find_food(sheep, world)
+@btime find_food($sheep, $world)
+
+#@code_warntype reproduce!(sheep, world)
+@btime reproduce!($sheep, $world)
