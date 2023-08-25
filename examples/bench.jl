55983831c4be3fb5def47827146035dc59773723
Niklas Heim <heim.niklas@gmail.com>
2023-08-25 14:59:01 +0200

    same bench script

--- examples/bench.jl
+++ examples/bench.jl
@@ -6,29 +6,47 @@ Random.seed!(0)
 sheep = Sheep(1,1,1,1,1,Female)
 sheep2 = Sheep(3001,1,1,1,1,Male)
 world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
+
 find_food(sheep,world) |> display
-
-sheep = Sheep(1,1,1,1,1,Female)
-sheep2 = Sheep(3001,1,1,1,1,Male)
-world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
-@code_warntype find_food(sheep, world)
-
-sheep = Sheep(1,1,1,1,1,Female)
-sheep2 = Sheep(3001,1,1,1,1,Male)
-world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
+#@code_warntype find_food(sheep, world)
 @btime find_food($sheep, $world)
 
-sheep = Sheep(1,1,1,1,1,Female)
-sheep2 = Sheep(3001,1,1,1,1,Male)
-world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
-reproduce!(sheep, world)
-
-sheep = Sheep(1,1,1,1,1,Female)
-sheep2 = Sheep(3001,1,1,1,1,Male)
-world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
-@code_warntype reproduce!(sheep, world)
-
-sheep = Sheep(1,1,1,1,1,Female)
-sheep2 = Sheep(3001,1,1,1,1,Male)
-world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
+#@code_warntype reproduce!(sheep, world)
 @btime reproduce!($sheep, $world)
+
+
+
+# using Ecosystem
+# using BenchmarkTools
+# using Random
+# Random.seed!(0)
+# 
+# sheep = Sheep(1,1,1,1,1,Female)
+# sheep2 = Sheep(3001,1,1,1,1,Male)
+# world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
+# find_food(sheep,world) |> display
+# 
+# sheep = Sheep(1,1,1,1,1,Female)
+# sheep2 = Sheep(3001,1,1,1,1,Male)
+# world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
+# @code_warntype find_food(sheep, world)
+# 
+# sheep = Sheep(1,1,1,1,1,Female)
+# sheep2 = Sheep(3001,1,1,1,1,Male)
+# world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
+# @btime find_food($sheep, $world)
+# 
+# sheep = Sheep(1,1,1,1,1,Female)
+# sheep2 = Sheep(3001,1,1,1,1,Male)
+# world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
+# reproduce!(sheep, world)
+# 
+# sheep = Sheep(1,1,1,1,1,Female)
+# sheep2 = Sheep(3001,1,1,1,1,Male)
+# world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
+# @code_warntype reproduce!(sheep, world)
+# 
+# sheep = Sheep(1,1,1,1,1,Female)
+# sheep2 = Sheep(3001,1,1,1,1,Male)
+# world = World(vcat([sheep,sheep2], [Grass(i) for i=2:3000]))
+# @btime reproduce!($sheep, $world)
